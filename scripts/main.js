0b112fd3331f70e152e5619cdda3eebee893c8b1
Koolxtreme <Koolxtreme@gmail.com>
2021-11-05 17:13:05 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menu-btn");
+const menuSlide = document.getElementById("menu")
+
+menuBtn.addEventListener("click", ()=>{
+    menuSlide.classList.toggle("show")    
+})
